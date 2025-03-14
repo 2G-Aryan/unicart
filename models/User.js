04c8ad960f753ba49657afb4af23b78a0aa25421
2G-Aryan <aryanupadhyay771@gmail.com>
2025-03-14 21:51:16 +0530

    update

--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
-
+// import { unique } from "next/dist/build/utils";
 
 const userSchema = new mongoose.Schema({
     _id:{ type: String, required: true},
     name:{ type: String, required: true},
-    email:{type: String, required: true, unique},
+    email:{type: String, required: true, unique:true},
     imageUrl : {type: String, required: true},
     cartItems: {type: Object, default:{}}
 },{minimize: false})
