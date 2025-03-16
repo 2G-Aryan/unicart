4fa2bce259b0f8612d70cb29cc92097851733ed4
Aryan Upadhyay <aryanupadhyay771@gmail.com>
2025-03-16 12:45:33 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,11 +4,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     _id:{ type: String, required: true},
     name:{ type: String, required: true},
-<<<<<<< HEAD
-    email:{type: String, required: true, unique:true},
-=======
     email:{type: String, required: true, unique: true},
->>>>>>> 9483ed0ed947d1db45aa04e23d43d5b19fe12116
     imageUrl : {type: String, required: true},
     cartItems: {type: Object, default:{}}
 },{minimize: false})
