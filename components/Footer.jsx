797255cd80291a6edf0eaf216419d26242170aab
Aryan Upadhyay <aryanupadhyay771@gmail.com>
2025-03-17 01:26:39 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -47,7 +47,7 @@ const Footer = () => {
         </div>
       </div>
       <p className="py-4 text-center text-xs md:text-sm">
-        Copyright 2025 © Unicart - Aryam/Arpit All Right Reserved.
+        Copyright 2025 © Unicart - Aryan/Arpit All Right Reserved.
       </p>
     </footer>
   );
